0120e859ea334c041d93173fb2b3a6cc3560018d
Liza Hainna <elizabeth.hastings121@gmail.com>
2023-05-15 11:05:40 -0300

    Começo do Blog Pessoal

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './components/estaticos/navbar/Navbar'
+import Footer from './components/estaticos/footer/Footer'
+import Home from './paginas/home/Home'
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+      <Home />
+    <Footer />
+    </>
+  )
+}
+
+export default App
